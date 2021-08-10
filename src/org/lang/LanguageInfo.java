d046096bdd41638abb4be8e9cdd7421d358b3055
kavinsaba <kavinsaba14@gmail.com>
2021-08-10 19:22:41 +0530

    My First

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,17 @@
+package org.lang;
+
+public class LanguageInfo {
+	public void tamilLanguage() {
+		System.out.println("tamil1");
+	}
+public void englishLanguage() {
+      System.out.println("english2");
+}
+	public void hindiLanguage() {
+     System.out.print("hindi3");
+    
+	}
+    
+
+}
+
---
+++ src/org/lang/StatesDeatils.java
@@ -0,0 +1,22 @@
+package org.lang;
+
+public class StatesDeatils {
+      
+	private void southIndia() {
+     System.out.print("tn");
+	} 
+ public void northIndia() {
+    System.out.print("Mh");
+ }
+public static void main(String[] args) {
+	StatesDeatils	kavi=new StatesDeatils();
+   kavi.southIndia();
+   kavi.northIndia();
+
+LanguageInfo lang=new LanguageInfo();
+lang.tamilLanguage();
+lang.englishLanguage();
+lang.hindiLanguage();
+
+}
+}
